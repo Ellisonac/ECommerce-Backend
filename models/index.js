7864de17f02637282c72cc85052665ad2a527335
Andrew Ellison <acannonellison@gmail.com>
2021-12-14 16:21:53 -0800

    Added nodemon package and relationships between models.

--- models/index.js
+++ models/index.js
@@ -6,22 +6,30 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  key: "category_id"
+  foreignKey: "category_id"
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-  key: "category_id"
+  foreignKey: "category_id"
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through:'ProductTag'
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: "tagged_products"
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through:'ProductTag'
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: "product_tags"
 })
 
 module.exports = {
